d832cb8792593b4129e838e9bb291a1c4f902509
UnivertyAndStuff <106083436+UnivertyAndStuff@users.noreply.github.com>
2022-06-02 08:48:04 +0800

    Update Fitness.cpp

--- Fitness.cpp
+++ Fitness.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 void info(){
+    // this functon contains the basic info and codition of the membership.
     cout << "Hello welcome to our Fitness membership gym. Plase enter the nessecary info below.\nThis is our membership condition elderly adult 30%, purchase membership 12 or more mothns is 15%, and pucrhase more than 5 session is 20%." << endl;
 }
 
+// this function is for the condition and calculates the membership
 void membershipRegister(){
     int age;
     int session;
@@ -25,46 +28,62 @@ void membershipRegister(){
         if (choice == 'a'){
             if(session > 5){
                 discount = 0.50;
-                discountPrice = ((cost * discount) - cost) * session; 
-                cout << "RM" << discountPrice << endl;
+                discountPrice = (cost - (cost * discount)) * session; 
+                std::cout << std::fixed;
+                std::cout << std::setprecision(2);
+                std::cout << "RM" << discountPrice << endl;
             }else{
                 discount = 0.3;
-                discountPrice = ((cost * discount) - cost) * session; 
-                cout << "RM" << discountPrice << endl;
+                discountPrice = (cost - (cost * discount)) * session;
+                std::cout << std::fixed;
+                std::cout << std::setprecision(2);
+                std::cout << "RM" << discountPrice << endl;
             }
             
         }else if (choice == 'b'){
             if(session > 5){
                 discount = 0.65;
-                discountPrice = ((cost * discount) - cost) * session; 
-                cout << "RM" << discountPrice << endl;
+                discountPrice = (cost - (cost * discount)) * session; 
+                std::cout << std::fixed;
+                std::cout << std::setprecision(2);
+                std::cout << "RM" << discountPrice << endl;
             }else{
                 discount = 0.45;
-                discountPrice = ((cost * discount) - cost) * session; 
-                cout << "RM" << discountPrice << endl;
+                discountPrice = (cost - (cost * discount)) * session; 
+                std::cout << std::fixed;
+                std::cout << std::setprecision(2);
+                std::cout << "RM" << discountPrice << endl;
             }
         }
     }else if (choice == 'b'){
          if(session > 5){
-                discount = 0.35;
-                discountPrice = ((cost * discount) - cost) * session; 
-                cout << "RM" << discountPrice << endl;
+            discount = 0.35;
+            discountPrice = (cost - (cost * discount)) * session; 
+            std::cout << std::fixed;
+            std::cout << std::setprecision(2);
+            std::cout << "RM" << discountPrice << endl;
         }else{
-                discount = 0.15;
-                discountPrice = ((cost * discount) - cost) * session; 
-                cout << "RM" << discountPrice << endl;
+            discount = 0.15;
+            discountPrice = (cost - (cost * discount)) * session; 
+            std::cout << std::fixed;
+            std::cout << std::setprecision(2);
+            std::cout << "RM" << discountPrice << endl;
         }
     }else if (session < 5){
         discount = 0.2;
-        discountPrice = ((cost * discount) - cost) * session; 
-        cout << "RM" << discountPrice << endl;
+        discountPrice = (cost - (cost * discount)) * session; 
+        std::cout << std::fixed;
+        std::cout << std::setprecision(2);
+         std::cout << "RM" << discountPrice << endl;
     }else{
-        cout << "RM" << cost * session << endl;
+        std::cout << std::fixed;
+        std::cout << std::setprecision(2);
+        std::cout << "RM" << discountPrice << endl;
     }
 }
 
 int main(){
+    // this are the called functions 
     info();
     membershipRegister();
-
 }
